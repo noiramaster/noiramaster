53917ee5c1a8fcfc18208c98aa613dab6cd9ce72
tit4naies-prog <tit4naies@gmail.COM>
2026-07-22 16:51:47 +0200

    Use Supabase pooler (eu-west-1) for IPv4 SQL connection

--- scripts/migrate-and-configure.js
+++ scripts/migrate-and-configure.js
@@ -8,8 +8,8 @@ const PROJECT_REF = SUPABASE_URL ? SUPABASE_URL.replace('https://', '').split('.
 async function runSQL(sql) {
   // Install pg at runtime
   const { Client } = require('pg')
-  const connString = `postgresql://postgres.${PROJECT_REF}:${encodeURIComponent(SUPABASE_KEY)}@db.${PROJECT_REF}.supabase.co:5432/postgres?sslmode=require`
-  const client = new Client({ connectionString: connString, family: 4 })
+  const connString = `postgresql://postgres.${PROJECT_REF}:${encodeURIComponent(SUPABASE_KEY)}@aws-0-eu-west-1.pooler.supabase.com:6543/postgres?pgbouncer=true&sslmode=require`
+  const client = new Client({ connectionString: connString })
   await client.connect()
   try {
     await client.query(sql)
